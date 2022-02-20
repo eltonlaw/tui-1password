63690e7cb43cac831b73af6a2e9352242552da81
Elton Law <eltonlaw296@gmail.com>
2022-02-19 20:42:37 -0500

    is_valid_cache used in getting session exit on invalid

--- src/op.rs
+++ src/op.rs
@@ -5,7 +5,7 @@ use std::error;
 use std::fs;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::process::Command;
+use std::process::{exit, Command};
 use std::str;
 use std::time::{Duration};
 use tracing;
@@ -15,7 +15,7 @@ static OP_TOKEN_TTL: u64 = 1800;
 
 /// True if cached token exists and created less than OP_TOKEN_TTL seconds ago
 /// FIXME: Use this
-pub fn is_valid_cache(path: &str) -> bool {
+pub fn is_valid_cache(path: &String) -> bool {
     if let Ok(metadata) = fs::metadata(path) {
         let token_age = metadata.modified().unwrap().elapsed().unwrap();
         return token_age < Duration::from_secs(OP_TOKEN_TTL);
@@ -60,9 +60,15 @@ pub fn home_dir() -> String {
 // FIXME: Should cache this
 pub fn get_session() -> Result<Session, Box<dyn error::Error>> {
     let op_token_path = format!("{}/token", home_dir());
-    let s = Session::from_cache(&op_token_path);
-    tracing::info!("Started new session: {} {:?}", &op_token_path, s);
-    return Ok(s);
+    if is_valid_cache(&op_token_path) {
+        let s = Session::from_cache(&op_token_path);
+        tracing::info!("Started new session: {} {:?}", &op_token_path, s);
+        return Ok(s);
+    } else {
+        tracing::error!("Failed to started new session, invalid 1password token {} ", &op_token_path);
+        println!("Not signed in. Please run `op signin > ~/.tui-1password/token`");
+        exit(1);
+    }
 }
 
 // FIXME: Refresh session
@@ -79,17 +85,8 @@ pub fn list_items() -> Result<Vec<Value>, serde_json::Error> {
                          .arg("list")
                          .arg("items")
                          .output().unwrap();
-    // FIXME: run check that stdout doesn't say something like "You are not currently signed in.
-    // Please run `op signin --help` for instructions\n"
     let items = str::from_utf8(&output.stdout).unwrap();
 
     serde_json::from_str(items)
-    // println!("output.status {:?}", output.status);
-    // println!("output.status.code {:?}", output.status.code());
-    // println!("LIST_ITEMS {:?}", items);
-    // let v: Value = serde_json::from_str(items).unwrap();
-    // println!("=======");
-    // // With each value we want to create a table that can be queried and fuzzy searched
-    // println!("{:?}", v[0]);
 
 }
