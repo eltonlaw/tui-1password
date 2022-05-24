02f2aa4ce066d9346a93cc01fec4338ab4cab208
Elton Law <eltonlaw296@gmail.com>
2022-05-23 22:07:20 -0400

    chore: unit tests + docstring for dec/inc_or_wrap

--- src/util.rs
+++ src/util.rs
@@ -1,24 +1,46 @@
+/// Arg `idx` incremented by 1, if past `max`, return 0
 pub fn inc_or_wrap(idx: Option<usize>, max: usize) -> usize {
-    match idx {
-        Some(i) => {
-            if i >= max - 1 {
-                0
-            } else {
-                i + 1
+    match max {
+        0 => 0,
+        _ => match idx {
+            Some(i) => {
+                if i >= max - 1 {
+                    0
+                } else {
+                    i + 1
+                }
             }
-        }
-        None => 0,
+            None => 0,
+        },
     }
 }
 
+/// Arg `idx` decremented by 1, if -1, return `max`
 pub fn dec_or_wrap(idx: Option<usize>, max: usize) -> usize {
-    match idx {
-        Some(i) => {
-            match i {
-                0 => max - 1,
-                _ => i - 1,
+    match max {
+        0 => 0,
+        _ => match idx {
+            Some(i) => {
+                match i {
+                    0 => max - 1,
+                    _ => i - 1,
+                }
             }
-        }
-        None => 0,
+            None => 0,
+        },
     }
+}
+
+#[test]
+fn inc_or_wrap_test() {
+    assert_eq!(inc_or_wrap(Some(1), 3), 2);
+    assert_eq!(inc_or_wrap(Some(2), 3), 0);
+    assert_eq!(inc_or_wrap(Some(0), 0), 0);
+}
+
+#[test]
+fn dec_or_wrap_test() {
+    assert_eq!(dec_or_wrap(Some(1), 3), 0);
+    assert_eq!(dec_or_wrap(Some(0), 3), 2);
+    assert_eq!(dec_or_wrap(Some(0), 0), 0);
 }
