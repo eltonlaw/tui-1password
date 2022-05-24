144853b02f1ea4fb82e8bf236c6fa2faf8b5ae4a
Elton Law <eltonlaw296@gmail.com>
2022-05-23 21:49:25 -0400

    update integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,15 +5,15 @@ use std::io::prelude::*;
 #[test]
 fn is_valid_cache() {
     // Invalid: file that doesn't exist
-    let not_exist_validity = op::is_valid_cache(&String::from("tests/resources/token-doesnt-exist"));
+    let not_exist_validity = op::Session::is_valid_cache(&String::from("tests/resources/token-doesnt-exist"));
     assert_eq!(false, not_exist_validity);
     // Invalid: old file
-    let expired_validity = op::is_valid_cache(&String::from("tests/resources/token-expired"));
+    let expired_validity = op::Session::is_valid_cache(&String::from("tests/resources/token-expired"));
     assert_eq!(false, expired_validity);
     // Valid: newly created file with export
     let mut file = File::create("tests/resources/token-new").unwrap();
     file.write_all(b"export OP_SESSION_my=\"f5uyNnTPFd_9K4RCsebDvV4MR-gX-t49x413vuIKbPM\"").unwrap();
-    let new_validity = op::is_valid_cache(&String::from("tests/resources/token-new"));
+    let new_validity = op::Session::is_valid_cache(&String::from("tests/resources/token-new"));
     assert_eq!(true, new_validity);
 }
 
