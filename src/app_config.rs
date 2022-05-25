f721b8cd5e7a10ac3d9e19a395f552752d9d0492
Elton Law <eltonlaw296@gmail.com>
2022-05-25 00:25:45 -0400

    custom config file

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,23 +1,42 @@
+use serde::{Serialize, Deserialize};
+use std::io::Write;
+use std::fs;
 use std::env;
+use tracing;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct AppConfig {
     pub headers: Vec<String>,
     pub root_dir: String,
-    pub is_debug: bool,
+    pub debug: bool,
     pub clipboard_bin: String,
 }
 
 impl AppConfig {
-    pub fn new() -> Self {
+    fn default() -> Self {
         let headers = vec![
             String::from("id"),
             String::from("title"),
             String::from("updated_at"),
         ];
         let root_dir = format!("{}/.tui-1password", env::var("HOME").unwrap());
-        let is_debug = true;
+        let debug = true;
         let clipboard_bin = String::from("wl-copy");
-        AppConfig { headers, root_dir, is_debug, clipboard_bin }
+        AppConfig { headers, root_dir, debug, clipboard_bin }
+    }
+    pub fn new() -> Self {
+        let config_fp = format!("{}/.tui-1password/tui-1password.yaml", env::var("HOME").unwrap());
+        if let Ok(_) = fs::metadata(&config_fp) {
+            tracing::info!("Config file read in from: {}", &config_fp);
+            let config_str = fs::read_to_string(&config_fp).unwrap();
+            return serde_yaml::from_str(config_str.as_str()).unwrap();
+        } else {
+            tracing::info!("No config file found generating new one: {}", config_fp);
+            let ac = AppConfig::default();
+            let s = serde_yaml::to_string(&ac).unwrap();
+            let mut f = fs::File::create(config_fp).unwrap();
+            f.write_all(s.as_bytes());
+            return ac;
+        }
     }
 }
