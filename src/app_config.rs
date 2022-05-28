fbd7ad7bbac49991ac994c772230a549ae5fe2da
Elton Law <eltonlaw296@gmail.com>
2022-05-28 13:39:29 -0400

    chore: convert .unwrap() to .unwrap_or_else()

--- src/app_config.rs
+++ src/app_config.rs
@@ -28,14 +28,18 @@ impl AppConfig {
         let config_fp = format!("{}/.tui-1password/tui-1password.yaml", env::var("HOME").unwrap());
         if fs::metadata(&config_fp).is_ok() {
             tracing::info!("Config file read in from: {}", &config_fp);
-            let config_str = fs::read_to_string(&config_fp).unwrap();
-            return serde_yaml::from_str(config_str.as_str()).unwrap();
+            let config_str = fs::read_to_string(&config_fp)
+                .unwrap_or_else(|err| panic!("Failed to read: {} - {}", &config_fp, err));
+            return serde_yaml::from_str(config_str.as_str())
+                .unwrap_or_else(|err| panic!("Couldn't deserialize: {} - {}", &config_fp, err));
         } else {
-            tracing::info!("No config file found generating new one: {}", config_fp);
+            tracing::info!("No config file found generating new one: {}", &config_fp);
             let ac = AppConfig::default();
-            let s = serde_yaml::to_string(&ac).unwrap();
-            let mut f = fs::File::create(config_fp).unwrap();
-            f.write_all(s.as_bytes());
+            let s = serde_yaml::to_string(&ac).expect("Failed to serialize default AppConfig");
+            let mut f = fs::File::create(&config_fp)
+                .unwrap_or_else(|err| panic!("Failed to create: {} - {}", &config_fp, err));
+            f.write_all(s.as_bytes())
+                .unwrap_or_else(|err| panic!("Failed to write contents: {} - {}", &config_fp, err));
             return ac;
         }
     }
